764e62072283b16d8970f809ce26137bfff031ac
Jilmar <veloza.jilmar.2393@eam.edu.co>
2024-10-01 23:22:28 -0500

    add (add class service user, service product)

--- taller_app_monolitica/src/main/java/com/example/taller_app_monolitica/service/serviceProduct.java
+++ taller_app_monolitica/src/main/java/com/example/taller_app_monolitica/service/serviceProduct.java
@@ -1,12 +1,27 @@
 package com.example.taller_app_monolitica.service;
 
+import com.example.taller_app_monolitica.model.Product;
+import com.example.taller_app_monolitica.model.User;
 import com.example.taller_app_monolitica.repository.RepositoryProduct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class serviceProduct {
     @Autowired
     private RepositoryProduct repositoryProduct;
 
+    public List<Product> findAll() {
+        return repositoryProduct.findAll();
+    }
+
+    public Product saveProduct(Product product) {
+        return repositoryProduct.save(product);
+    }
+
+    public void deleteProduct(Long id) {
+        repositoryProduct.deleteById(id);
+    }
 }
--- taller_app_monolitica/src/main/java/com/example/taller_app_monolitica/service/serviceUser.java
+++ taller_app_monolitica/src/main/java/com/example/taller_app_monolitica/service/serviceUser.java
@@ -1,13 +1,27 @@
 package com.example.taller_app_monolitica.service;
 
+import com.example.taller_app_monolitica.model.User;
 import com.example.taller_app_monolitica.repository.RepositoryUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class serviceUser {
-@Autowired
+    @Autowired
     private RepositoryUser repositoryUser;
 
+    public List<User> findAll() {
+        return repositoryUser.findAll();
+    }
+
+    public User saveUser(User user) {
+        return repositoryUser.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        repositoryUser.deleteById(id);
+    }
 }
